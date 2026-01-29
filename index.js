127c59b6e17d1ac0245b2f5446878238fdb80fc9
YasminRage <yasmin_rage@outlook.com>
2026-01-29 23:58:29 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 import app from './server.js'
 import mongodb from "mongodb"
 import dotenv from "dotenv"
+import MoviesDAO from './dao/moviesDAO.js'
 
-async function main () {
+async function main() {
     dotenv.config()
 
     const client = new mongodb.MongoClient(
         process.env.MOVIEREVIEWS_DB_URI
     )
+
     const port = process.env.PORT || 8000
-
     try {
-        //Connnect to MongoDB Cluster
         await client.connect()
-
-        app.listen(port, () =>{
-            console.log('server is runing on port:'+port);
+        await MoviesDAO.injectDB(client)
+        app.listen(port, () => {
+            console.log('server is running on port:' + port);
         })
+
     } catch (e) {
         console.error(e);
         process.exit(1)
